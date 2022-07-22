0e991a016eed1cd98e56ee246ae36d3c08a627b4
TunamiRider <yoite.sy@gmail.com>
2022-07-22 10:06:28 -0500

    This is a practice for  ios app
    Target version: ios version 15
    tool: xcode13

---
+++ Restart-App/Restart-App/BetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetaApp.swift
+//  Beta
+//
+//  Created by Yuzu on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct BetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
