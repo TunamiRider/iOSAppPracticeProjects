2f4303e886746f67643ee231ee61252e7c7d5fa1
TunamiRider <yoite.sy@gmail.com>
2022-07-28 10:48:35 -0500

    first commit

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Yuzu on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
