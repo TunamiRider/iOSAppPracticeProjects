323161c9bc99c9c9aac9a788029eab127ed35997
TunamiRider <yoite.sy@gmail.com>
2022-08-02 16:02:42 -0500

    Added scroll view for helmets with lazy vertical  grid (fifth commit)

---
+++ Touchdown/Touchdown/View/Home/TitleView.swift
@@ -0,0 +1,33 @@
+//
+//  TitleView.swift
+//  Touchdown
+//
+//  Created by Yuzu on 8/2/22.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    // MARK: - PROPERTIES
+    var title: String
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+            Spacer()
+        } //: HSTACK
+        .padding(.horizontal)
+        .padding(.top, 15)
+        .padding(.bottom, 10)
+    }
+}
+
+struct TitleView_Previews: PreviewProvider {
+    static var previews: some View {
+        TitleView(title: "Helmet")
+            .previewLayout(.sizeThatFits)
+            .background(colorBackground)
+    }
+}
