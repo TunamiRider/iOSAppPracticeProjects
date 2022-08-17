a2d83d2794e29d1b980ce8612fc183b6bf6e3c05
TunamiRider <yoite.sy@gmail.com>
2022-08-17 14:24:05 -0500

    Added brand grid view(seventh commit)

---
+++ Touchdown/Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Yuzu on 8/17/22.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
