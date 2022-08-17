c76d3e9fe7801e31fd84ec623ba09ce59b5be01e
TunamiRider <yoite.sy@gmail.com>
2022-08-17 15:31:24 -0500

    Added product detail view(seventh commit)

---
+++ Touchdown/Touchdown/App/ProductDetailView.swift
@@ -0,0 +1,73 @@
+//
+//  ProductDetailView.swift
+//  Touchdown
+//
+//  Created by Yuzu on 8/17/22.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5, content: {
+            // NAVBAR
+            NavigationBarDetailView()
+                .padding(.horizontal)
+                .padding(.top,
+                         UIApplication.shared.windows.first?.safeAreaInsets.top)
+            // HEADER
+            HeaderDetailView()
+                .padding(.horizontal)
+            
+            // DETAL TOP PART
+            TopPartDetailView()
+                .padding(.horizontal)
+                .zIndex(1)
+            
+            // DETAL BOTTOM PART
+            VStack(alignment: .leading, spacing: 0, content: {
+                // RATINGS + FAVORITE
+                RatingSizesDetailView()
+                    .padding(.top, -20)
+                    .padding(.bottom, 10)
+                
+                // DESCRIPTION
+                ScrollView(.vertical, showsIndicators: false, content: {
+                    Text(sampleProduct.description)
+                        .font(.system(.body, design: .rounded))
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                }) //: SCROLL
+                
+                // QUANTITY + FAVORITE
+                QuantityFavoriteDetailView()
+                    .padding(.vertical, 10)
+                // ADD TO CART
+                AddToCartDetailView()
+                    .padding(.bottom, 20)
+                Spacer()
+            }) //: VSTACK
+            .padding(.horizontal)
+            .background(
+                Color.white
+                    .clipShape(CustomShape())
+                    .padding(.top, -105)
+            )
+        }) //: VSTACK
+        .zIndex(0)
+        .ignoresSafeArea(.all, edges: .all)
+        .background(
+            Color(red: sampleProduct.red,
+                  green: sampleProduct.green,
+                  blue: sampleProduct.blue
+                 ).ignoresSafeArea(.all, edges: .all)
+        )
+    }
+}
+
+struct ProductDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductDetailView()
+            .previewLayout(.fixed(width: 375, height: 812))
+    }
+}
