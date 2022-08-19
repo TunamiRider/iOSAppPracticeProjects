d5c029cd044fcab2d8e82c41624ae75a6cb6c1d3
TunamiRider <yoite.sy@gmail.com>
2022-08-19 15:00:42 -0500

    Rearrange persistent model (2th commit)

---
+++ Devote/Devote/Utility/Constant.swift
@@ -0,0 +1,21 @@
+//
+//  Constant.swift
+//  Devote
+//
+//  Created by Yuzu on 8/19/22.
+//
+
+import Foundation
+
+// MARK: - FORMATTER
+
+let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+// MARK: - UI
+
+// MARK: - UX
