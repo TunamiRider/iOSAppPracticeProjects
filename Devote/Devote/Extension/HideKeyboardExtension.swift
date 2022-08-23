edddfeeadc5b79ec5a13bcc75be9b0bc8de63037
TunamiRider <yoite.sy@gmail.com>
2022-08-23 14:37:39 -0500

    updated keyboard feature (3rd commit)

---
+++ Devote/Devote/Extension/HideKeyboardExtension.swift
@@ -0,0 +1,17 @@
+//
+//  HideKeyboardExtension.swift
+//  Devote
+//
+//  Created by Yuzu on 8/23/22.
+//
+
+import Foundation
+import SwiftUI
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard(){
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
--- Devote/Devote/View/ContentView.swift
+++ Devote/Devote/View/ContentView.swift
@@ -11,6 +11,12 @@ import CoreData
 struct ContentView: View {
     // MARK: - PROPERTY
     
+    @State var task: String = ""
+    
+    private var isButtonDisabled: Bool {
+        task.isEmpty
+    }
+    
     // FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
 
@@ -24,13 +30,19 @@ struct ContentView: View {
         withAnimation {
             let newItem = Item(context: viewContext)
             newItem.timestamp = Date()
-
+            newItem.task = task
+            newItem.completion = false
+            newItem.id = UUID()
+            
             do {
                 try viewContext.save()
             } catch {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
+            
+            task = ""
+            hideKeyboard()
         }
     }
 
@@ -51,29 +63,54 @@ struct ContentView: View {
 
     var body: some View {
         NavigationView {
-
+            VStack{
+                VStack(spacing: 16){
+                    TextField("New Task", text: $task)
+                        .padding()
+                        .background(
+                            Color(UIColor.systemGray6)
+                        )
+                        .cornerRadius(10)
+                    
+                    Button(action: {
+                        addItem()
+                    }, label: {
+                        Spacer()
+                        Text("SAVE")
+                        Spacer()
+                    })
+                    .disabled(isButtonDisabled)
+                    .padding()
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .background(isButtonDisabled ? Color.gray : Color.pink)
+                    .cornerRadius(10)
+                } //: VSTACK
+                .padding()
+                
                 List {
                     ForEach(items) { item in
-                        NavigationLink {
-                            Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                        } label: {
-                            Text(item.timestamp!, formatter: itemFormatter)
-                        }
+
+                            VStack(alignment: .leading) {
+                                Text(item.task ?? "")
+                                    .font(.headline)
+                                    .fontWeight(.bold)
+                                
+                                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                                    .font(.footnote)
+                                    .foregroundColor(.gray)
+                            } //: LIST ITEM
                     }
                     .onDelete(perform: deleteItems)
                 } //: LIST
+            } //: VSTACK
+            .navigationBarTitle("Daily Tasks", displayMode: .large)
                 .toolbar {
                     #if os(iOS)
                     ToolbarItem(placement: .navigationBarTrailing) {
                         EditButton()
                     }
                     #endif
-                    
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button(action: addItem) {
-                            Label("Add Item", systemImage: "plus")
-                        }
-                    }
             }//: toolbar
 
             Text("Select an item")
