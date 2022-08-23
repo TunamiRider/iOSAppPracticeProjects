ec18c31bbee7ef6585471725dfc087231becaa2d
TunamiRider <yoite.sy@gmail.com>
2022-08-23 17:28:30 -0500

    Added blurr effect on newTaskView with animation (eight commit)

--- Devote/Devote/View/ContentView.swift
+++ Devote/Devote/View/ContentView.swift
@@ -133,11 +133,14 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                  } //: VSTACK
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
                 
                     // MARK: - NEW TASK ITEM
                 
                     if showNewTaskItem {
-                        BlankView()
+                        BlankView(backgroundColor: isDarkMode ? Color.black : Color.gray, backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                             .onTapGesture {
                                 withAnimation() {
                                     showNewTaskItem = false
@@ -160,6 +163,7 @@ struct ContentView: View {
 //                } //: toolbar
                 .background(
                     BackgroundImageView()
+                        .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
                 )
                 .background(
                     backgroundGradient.ignoresSafeArea(.all)
