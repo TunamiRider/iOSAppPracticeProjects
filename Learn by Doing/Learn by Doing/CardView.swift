9ba936e9a2e70836639b82407eea9c14175ceaed
TunamiRider <yoite.sy@gmail.com>
2022-09-01 14:04:25 -0500

    Added contentView with scene delegate, animation/sound, and stickers (completed)

--- Learn by Doing/Learn by Doing/CardView.swift
+++ Learn by Doing/Learn by Doing/CardView.swift
@@ -12,9 +12,17 @@ struct CardView: View {
     
     var card: Card
     
+    @State private var fadeIn: Bool = false
+    @State private var moveDownward: Bool = false
+    @State private var moveUpward: Bool = false
+    @State private var showAlert: Bool = false
+    
+    var hapticImpact = UIImpactFeedbackGenerator(style: .heavy)
+    
     var body: some View {
         ZStack {
             Image(card.imageName)
+                .opacity(fadeIn ? 1.0 : 0.0)
             
             VStack {
                 Text(card.title)
@@ -27,12 +35,16 @@ struct CardView: View {
                     .foregroundColor(Color.white)
                     .italic()
             }
-            .offset(y: -218)
+            .offset(y: moveDownward ? -218 : -300)
             
             Button(action: {
                 print("Button was tapped.")
                 
                 playSound(sound: "sound-chime", type: "mp3")
+            
+                self.hapticImpact.impactOccurred()
+                self.showAlert.toggle()
+                
             }, label: {
                 HStack {
                     Text(card.callToAction.uppercased())
@@ -52,12 +64,26 @@ struct CardView: View {
 
                 
             })
-            .offset(y: 210)
+            .offset(y: moveUpward ? 210 : 300)
         }
         .frame(width: 335, height: 545)
         .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(16)
         .shadow(radius: 8)
+        .onAppear(){
+            withAnimation(.linear(duration: 1.2)){
+                self.fadeIn.toggle()
+            }
+            withAnimation(.linear(duration: 0.8)){
+                self.moveDownward.toggle()
+                self.moveUpward.toggle()
+            }
+        }
+        .alert(isPresented: $showAlert){
+            Alert(title: Text(card.title),
+                  message: Text(card.message),
+                  dismissButton: .default(Text("OK")))
+        }
     }
 }
 
