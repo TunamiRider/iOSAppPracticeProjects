9f3d754bd7ba6d524459a9ea553914943a8bbb93
TunamiRider <yoite.sy@gmail.com>
2022-09-09 15:32:52 -0500

    Set up for app view (second commit)

---
+++ Avocados/Avocados/View/AvocadosView.swift
@@ -0,0 +1,20 @@
+//
+//  AvocadosView.swift
+//  Avocados
+//
+//  Created by Yuzu on 9/9/22.
+//
+
+import SwiftUI
+
+struct AvocadosView: View {
+    var body: some View {
+        Text("Avocados")
+    }
+}
+
+struct AvocadosView_Previews: PreviewProvider {
+    static var previews: some View {
+        AvocadosView()
+    }
+}
---
+++ Avocados/Avocados/View/RipeningStageView.swift
@@ -0,0 +1,20 @@
+//
+//  RipeningStageView.swift
+//  Avocados
+//
+//  Created by Yuzu on 9/9/22.
+//
+
+import SwiftUI
+
+struct RipeningStageView: View {
+    var body: some View {
+        Text("Ripening!")
+    }
+}
+
+struct RipeningStageView_Previews: PreviewProvider {
+    static var previews: some View {
+        RipeningStageView()
+    }
+}
