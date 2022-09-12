3db6ca371895cbd151128d43e7e0661a6f005e18
TunamiRider <yoite.sy@gmail.com>
2022-09-12 11:43:39 -0500

    Added scroll view and its fact views (6th commit)

--- Avocados/Avocados/View/ContentView.swift
+++ Avocados/Avocados/View/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     // MARK: - PROPERTIES
     var headers: [Header] = headerData
+    var facts: [Fact] = factData
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
@@ -31,6 +32,22 @@ struct ContentView: View {
                 
                 DishesView()
                     .frame(maxWidth: 640)
+                
+                // MARK: -AVOCADO FACTS
+                Text("Avocado Facts")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                ScrollView(.horizontal, showsIndicators: false){
+                    HStack(alignment: .top, spacing: 60){
+                        ForEach(facts) {item in
+                            FactsView(fact: item)
+                        }
+                    }
+                    .padding(.vertical)
+                    .padding(.leading, 60)
+                    .padding(.trailing, 20)
+                }
                 
                 // MARK: - FOOTER
                 
@@ -64,6 +81,6 @@ struct TitleModifier: ViewModifier {
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(headers: headerData)
+        ContentView(headers: headerData, facts: factData)
     }
 }
