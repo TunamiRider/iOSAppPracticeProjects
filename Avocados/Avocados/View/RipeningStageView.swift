0a30ebb9dddc7cadc44b8e5f15a80a507901394b
TunamiRider <yoite.sy@gmail.com>
2022-09-12 15:27:32 -0500

    Added simple setting view (completed this project)

--- Avocados/Avocados/View/RipeningStageView.swift
+++ Avocados/Avocados/View/RipeningStageView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct RipeningStageView: View {
+    var ripeningStages: [Ripening] = ripeningData
+    
     var body: some View {
-        Text("Ripening!")
+        ScrollView(.horizontal, showsIndicators: false) {
+            VStack {
+                Spacer()
+                HStack(alignment: .center, spacing: 25) {
+                    ForEach(ripeningStages){ item in
+                        RipeningView(ripening: item)
+                    }
+                }
+                .padding(.vertical)
+                .padding(.horizontal, 20)
+                Spacer()
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
 struct RipeningStageView_Previews: PreviewProvider {
     static var previews: some View {
-        RipeningStageView()
+        RipeningStageView(ripeningStages: ripeningData)
     }
 }
