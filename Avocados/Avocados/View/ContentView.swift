dbb2b1b229ca3cffe92d003f3b5d2a2037a85659
TunamiRider <yoite.sy@gmail.com>
2022-09-12 12:15:06 -0500

    Added recipes views (7th commit)

--- Avocados/Avocados/View/ContentView.swift
+++ Avocados/Avocados/View/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     // MARK: - PROPERTIES
     var headers: [Header] = headerData
     var facts: [Fact] = factData
+    var recipes: [Recipe] = recipesData
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
@@ -49,6 +50,20 @@ struct ContentView: View {
                     .padding(.trailing, 20)
                 }
                 
+                // MARK: - RECIPE CARDS
+                Text("Avocado Recipes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                VStack(alignment: .center, spacing: 20){
+                    ForEach(recipes) {item in
+                        RecipeCardView(recipe: item)
+                    }
+                }
+                .frame(maxWidth: 640)
+                .padding(.horizontal)
+                
+                
                 // MARK: - FOOTER
                 
                 VStack(alignment: .center, spacing: 20) {
@@ -81,6 +96,6 @@ struct TitleModifier: ViewModifier {
 }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView(headers: headerData, facts: factData)
+        ContentView(headers: headerData, facts: factData, recipes: recipesData)
     }
 }
---
+++ Avocados/Avocados/View/RecipeCardView.swift
@@ -0,0 +1,86 @@
+//
+//  RecipeCardView.swift
+//  Avocados
+//
+//  Created by Yuzu on 9/12/22.
+//
+
+import SwiftUI
+
+struct RecipeCardView: View {
+    
+    var recipe: Recipe
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            // CARD IMAGE
+            Image(recipe.image)
+                .resizable()
+                .scaledToFit()
+                .overlay(
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Image(systemName: "bookmark")
+                                .font(Font.title.weight(.light))
+                                .foregroundColor(Color.white)
+                                .imageScale(.small)
+                            .shadow(color: Color("ColorBlackTransparentLight"), radius: 2, x: 0, y: 0)
+                            .padding(.trailing, 20)
+                            .padding(.top, 22)
+                            Spacer()
+                        }
+                    }
+                )
+            
+            VStack(alignment: .leading, spacing: 12){
+                Text(recipe.title)
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenMedium"))
+                    .lineLimit(1)
+                
+                Text(recipe.headline)
+                    .font(.system(.title, design: .serif))
+                    .foregroundColor(Color.gray)
+                    .italic()
+                
+                HStack(alignment: .center, spacing: 5) {
+                    ForEach(1...(recipe.rating), id: \.self) { _ in
+                        Image(systemName: "star.fill")
+                            .foregroundColor(Color.yellow)
+                    }
+                }
+                
+                HStack(alignment: .center, spacing: 12){
+                    HStack(alignment: .center, spacing: 2){
+                        Image(systemName: "person.2")
+                        Text("Serves: \(recipe.serves)")
+                    }
+                    HStack(alignment: .center, spacing: 2){
+                        Image(systemName: "clock")
+                        Text("Prep: \(recipe.preparation)")
+                    }
+                    HStack(alignment: .center, spacing: 2){
+                        Image(systemName: "flame")
+                        Text("Cooking: \(recipe.cooking)")
+                    }
+                }
+                .font(.footnote)
+                .foregroundColor(Color.gray)
+            }
+            .padding()
+            .padding(.bottom, 12)
+        }
+        .background(Color.white)
+        .cornerRadius(12)
+        .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 0)
+    }
+}
+
+struct RecipeCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCardView(recipe: recipesData[0])
+            .previewLayout(.sizeThatFits)
+    }
+}
