1607730a4006b1f42b466d83b61802a89152d6d7
TunamiRider <yoite.sy@gmail.com>
2022-09-14 15:13:18 -0500

    set up (#1)

---
+++ Slot Machine/Slot Machine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Slot Machine
+//
+//  Created by Yuzu on 9/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
