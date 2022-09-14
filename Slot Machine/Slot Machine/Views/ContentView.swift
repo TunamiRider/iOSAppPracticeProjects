7f828ab7f750960f3fc19293d233750c08b61ce5
TunamiRider <yoite.sy@gmail.com>
2022-09-14 15:46:12 -0500

    Added basic images on contentView (#3)

--- Slot Machine/Slot Machine/Views/ContentView.swift
+++ Slot Machine/Slot Machine/Views/ContentView.swift
@@ -9,7 +9,106 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // MARK: - BACKGROUND
+            
+            LinearGradient(gradient: Gradient(colors: [Color("ColorPink"), Color("ColorPurple")]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            // MARK: - INTERFACE
+            
+            VStack(alignment: .center, spacing: 5) {
+                // MARK: - HEADER
+                
+                LogoView()
+                
+                Spacer()
+                
+                // MARK: - SCORE
+                
+                HStack {
+                  HStack {
+                    Text("Your\nCoins".uppercased())
+                      .scoreLabelStyle()
+                      .multilineTextAlignment(.trailing)
+                    
+                    Text("100")
+                      .scoreNumberStyle()
+                      .modifier(ScoreNumberModifier())
+                  }
+                  .modifier(ScoreContainerModifier())
+                  
+                  Spacer()
+                  
+                  HStack {
+                    Text("200")
+                      .scoreNumberStyle()
+                      .modifier(ScoreNumberModifier())
+                    
+                    Text("High\nScore".uppercased())
+                      .scoreLabelStyle()
+                      .multilineTextAlignment(.leading)
+                    
+                  }
+                  .modifier(ScoreContainerModifier())
+                }
+                
+                // MARK: - SLOT MACHINE
+                VStack(alignment: .center, spacing: 0) {
+                    // MARK: - REEL #1
+                    ZStack {
+                      ReelView()
+                            .modifier(ImageModifier())
+                    }
+                    HStack(alignment: .center, spacing: 0) {
+                      // MARK: - REEL #2
+                      ZStack {
+                        ReelView()
+                      }
+                      
+                      Spacer()
+                      
+                      // MARK: - REEL #3
+                      ZStack {
+                        ReelView()
+                      }
+                    }
+                    .frame(maxWidth: 500)
+                    
+                    // MARK: - SPIN BUTTON
+                    Button(action: {
+                    }) {
+                      Image("gfx-spin")
+                        .resizable()
+                        .modifier(ImageModifier())
+                    }
+                } // Slot Machine
+                 .layoutPriority(2)
+
+            }
+            // MARK: - BUTTONS
+            .overlay(
+              // RESET
+              Button(action: {
+                
+              }) {
+                Image(systemName: "arrow.2.circlepath.circle")
+                  .foregroundColor(.white)
+              }
+              .modifier(ButtonModifier()),
+              alignment: .topLeading
+            )
+            .overlay(
+              // INFO
+              Button(action: {
+                
+              }) {
+                Image(systemName: "info.circle")
+                  .foregroundColor(.white)
+              }
+              .modifier(ButtonModifier()),
+              alignment: .topTrailing
+            )
+        }
     }
 }
 
